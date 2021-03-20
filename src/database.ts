5ea3ac167ac48553d92555adda457c0285349567
fin calfin <muhammadcalfin08@gmail.com>
2021-03-20 12:06:45 +0700

    change database

--- src/database.ts
+++ src/database.ts
@@ -5,13 +5,13 @@ dotenv.config()
 
 let url: string | undefined
 
-if (process.env.NODE_ENV === 'dev') {
-    url = process.env.MONGO_URI_TEST
-} else {
-    url = process.env.MONGO_URI
-}
+// if (process.env.NODE_ENV === 'dev') {
+//     url = process.env.MONGO_URI_TEST
+// } else {
+//     url = process.env.MONGO_URI
+// }
 consola.log(url, 'ini urlny db')
-mongoose.connect(url || "", {
+mongoose.connect('mongodb+srv://calfin08:codered2132@betulin.jvruk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(db => {
