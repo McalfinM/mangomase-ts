939d3fb1b04065e9f13e3b4402da7a2278a6a275
fin calfin <muhammadcalfin08@gmail.com>
2021-04-07 23:52:52 +0700

    feature authorization comment

--- build/database.js
+++ build/database.js
@@ -4,16 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-let url;
-if (process.env.NODE_ENV === 'dev') {
-    url = process.env.MONGO_URI_TEST;
-}
-else {
-    url = process.env.MONGO_URI;
-}
-mongoose_1.default.connect(url || '', {
+// let url: string | undefined
+// if (process.env.NODE_ENV === 'dev') {
+//     url = process.env.MONGO_URI_TEST
+// } else {
+//     url = process.env.MONGO_URI
+// }
+mongoose_1.default.connect('mongodb+srv://calfin08:codered2132@betulin.jvruk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(db => {
