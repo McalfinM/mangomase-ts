b283728d1f3ff79a6dc9be44b38f68d80b12456e
calfin08 <muhammadcalfin08@gmail.com>
2022-02-11 09:44:50 +0700

    change create can carts and add menu

--- src/middleware/validator/postCreate.ts
+++ src/middleware/validator/postCreate.ts
@@ -4,5 +4,8 @@ export const createCartValidator = (): ValidationChain[] => {
     return [
 
         body('customer_name', 'Masukan nama kamu').exists().notEmpty().default('client' + new Date()),
+        body('carts', 'Carts harus berbentuk array').isArray(),
+        body('carts.*.menu_uuid', 'Menu tidak boleh kosong').isUUID(),
+        body('carts.*.quantity', 'Quantity tidak boleh kosong').isNumeric(),
     ]
 }
